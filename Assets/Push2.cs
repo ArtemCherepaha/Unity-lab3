e3f32f1aa467deea3417eced33af90076d95bbd0
Cherepaha <artem322cherepaha@gmail.com>
2021-04-01 16:09:21 +0300

    lab

---
+++ Assets/Push2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push2 : MonoBehaviour
+{
+    private GameObject Pushtext;
+    void Start()
+    {
+        Pushtext = GameObject.Find("Pushtext2");
+        Pushtext.gameObject.SetActive(false);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pushtext.gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pushtext.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Push3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push3 : MonoBehaviour
+{
+    private GameObject Pushtext;
+    void Start()
+    {
+        Pushtext = GameObject.Find("Pushtext3");
+        Pushtext.gameObject.SetActive(false);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pushtext.gameObject.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pushtext.gameObject.SetActive(false);
+        }
+    }
+}
